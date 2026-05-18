4375ce3d89fc158964bea00d758175958d0533f3
Nasha <nashatvvm@gmail.com>
2026-05-18 23:40:14 +0530

    Quiz Project

---
+++ questions.js
@@ -0,0 +1,31 @@
+const questions = [
+    {
+        question: "Who is the chief minister of keralaa?",
+        options : ["V.D Satheesan", "Ramesh Chennithala", "K.C Venugopal", "Pinarayi Vijayan"],
+        answer: "V.D Satheesan"
+    },
+
+    {
+        question: "Which district is known as the IT hub of kerala?",
+        options : ["Kannur", "Kollam", "Thiruvananthapuram", "Wayanad"],
+        answer: "Thiruvananthapuram"
+    },
+
+    {
+        question: "Which Kerala IT park is located in Kochi?",
+        options : ["Technopark", "Infopark", "Cyberpark", "Kinfra Park"],
+        answer: "Infopark"
+    },
+
+    {
+        question: "Which Kerala project aims to provide high-speed internet to all households?",
+        options : ["K-FON", "Digital Kerala", "Kerala Net", "Fibre Mission"],
+        answer: "K-FON"
+    },
+
+    {
+        question: "Which Kerala government initiative focuses on digital governance services?",
+        options : ["Akshaya", "e-Kerala", "Smart Kerala", "Digital Kerala"],
+        answer: "e-Kerala"
+    }
+];
---
+++ script.js
@@ -0,0 +1,75 @@
+let currentQuestion = 0;
+let score = 0;
+
+function loadQuestion() {
+
+  const q = questions[currentQuestion];
+
+  document.getElementById("question").innerHTML =
+    q.question;
+
+  let optionsHtml = "";
+
+  q.options.forEach(option => {
+
+    optionsHtml += `
+      <button onclick="checkAnswer('${option}')">
+        ${option}
+      </button>
+      <br><br>
+    `;
+  });
+
+  document.getElementById("options").innerHTML =
+    optionsHtml;
+}
+function checkAnswer(option) {
+
+  const buttons = document.querySelectorAll("#options button");
+
+  buttons.forEach(btn => {
+
+    if(btn.innerText === questions[currentQuestion].answer) {
+      btn.style.background = "#056d0e";
+    }
+
+    if(btn.innerText === option && option !== questions[currentQuestion].answer){
+      btn.style.background = "#b11005";
+    }
+
+    btn.disabled = true;
+  });
+
+  if(option === questions[currentQuestion].answer) {
+    score++;
+  }
+}
+
+function nextQuestion() {
+
+  currentQuestion++;
+
+  if(currentQuestion < questions.length) {
+    loadQuestion();
+  }
+  else {
+
+    if(score === questions.length) {
+
+        document.body.innerHTML =
+        `<h1>Congratulations! 🎉</h1>
+         <h2>You got full marks: ${score}/${questions.length}</h2>`;
+
+    }
+
+    else {
+
+        document.body.innerHTML =
+        `<h1>Your Score: ${score}/${questions.length}</h1>`;
+
+    }
+
+  }
+}
+
+loadQuestion();
